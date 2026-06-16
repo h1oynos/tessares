a6d7926878d43aa5f25eab07370cfaadf7471d26
Jordan Mantel <survergd@gmail.com>
2026-06-16 18:38:55 -0400

    Set up some empty files and directory structure

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # TesSaRes
-**New Block Stacking Game**
-\* README is currently a work in progress, please wait...
+## What is TesSaRes?
+TesSaRes is a project that aims to provide a block stacking experience for everyone, whether a beginner, advanced, or grandmaster-level in the genre. 
+
+# Ways to Play
+*Note: Some modes listed here may not be in the game, and will be marked as such.*
+
+## Classic
+*Methods of play you can find just about anywhere else. The classic block stacking experience*
+* **40 Lines** -- Clear 40 lines as fast as possible.
+* **Blitz** -- Get as many points as possible in 2 minutes.
+* **Endless** -- Play TesSaRes for as long as you can survive.
+
+## Skill Analysis
+*Methods of play that analyze your skill and assign you a grade.*
+* **Easy** -- An easy mode with no time pressure. Get enough lights to pass each section!
+* **Spectrum** -- The regular Skill Analysis-style gameplay. Can you become a Grandmaster?
+* **3i** -- A version of play with a speed curve that changes depending on your own skill.
+* **Maximum** -- A super hard mode. *20G* all the time!
+* **And more...**
+
+# Credits
+| Responsibility     | Name   |
+|--------------------|--------|
+| Programming        | Kyurei |
+| Graphics           | Kyurei |
+| Concepts           | --     |
+| Music              | --     |
+| Playtesting        | --     |
+| Special Thanks     | --     |
+
+* Currently accepting musicians to contribute their existing works or original works to use as music. Please contact me via the methods given at https://kyureimain.carrd.co if interested.
+
---
+++ main.py
@@ -0,0 +1,98 @@
+import pygame
+import pygame_gui as gui
+import numpy as np
+
+
+# Setting up Pygame
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+# Setting up the game
+
+matrix = np.zeros((22, 10))
+def draw_matrix():
+    row_no = 0
+    cell_no = 0
+    for row in matrix:
+        cell_no = 0
+        row_pixels_added = row_no * 25 # Number of pixels to shift down the Y position of the cell
+        for cell in row:
+            cell_pixels_added = cell_no * 25 # Number of pixels to shift right the X position of the cell
+            match cell:
+                case 0: # Empty Cell
+                    if row_no < 2:
+                        break
+                    pygame.draw.rect(screen, pygame.Color(46, 40, 42), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 1: # L
+                    pygame.draw.rect(screen, pygame.Color(186, 92, 18), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 2: # J
+                    pygame.draw.rect(screen, pygame.Color(30, 56, 136), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 3: # S
+                    pygame.draw.rect(screen, pygame.Color(255, 209, 102), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 4: # Z
+                    pygame.draw.rect(screen, pygame.Color(226, 52, 40), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 5: # O
+                    pygame.draw.rect(screen, pygame.Color(214, 190, 31), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 6: # I
+                    pygame.draw.rect(screen, pygame.Color(71, 168, 189), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case 7: # T
+                    pygame.draw.rect(screen, pygame.Color(157+20, 105+20, 163+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -1: # Active L
+                    pygame.draw.rect(screen, pygame.Color(186+20, 92+20, 18+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -2: # Active J
+                    pygame.draw.rect(screen, pygame.Color(30+20, 56+20, 136+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -3: # Active S
+                    pygame.draw.rect(screen, pygame.Color(255+20, 209+20, 102+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -4: # Active Z
+                    pygame.draw.rect(screen, pygame.Color(226+20, 52+20, 40+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -5: # Active O
+                    pygame.draw.rect(screen, pygame.Color(214+20, 190+20, 31+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -6: # Active I
+                    pygame.draw.rect(screen, pygame.Color(71+20, 168+20, 189+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+                case -7: # Active T
+                    pygame.draw.rect(screen, pygame.Color(157+20, 105+20, 163+20), pygame.rect.FRect(200+cell_pixels_added, 50+row_pixels_added, 24, 24))
+
+                
+            cell_no += 1
+        row_no += 1
+
+class Piece:
+    def __init__(self, shape):
+        self.type = shape
+        match shape:
+            case 1: # L
+                pass
+            case 2: # J
+                pass
+            case 3: # S
+                pass
+            case 4: # Z
+                pass
+            case 5: # O
+                pass
+            case 6: # I
+                pass
+            case 7: # T
+                pass
+
+
+while running:
+    # Event Polling Loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    ## Begin Rendering Loop 
+
+    draw_matrix()
+    ## End Rendering Loop
+
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
